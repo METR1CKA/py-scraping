e4c3ba882665e9da03e91ff9e2aa748725367bf3
METR1CKA <ferchosalazar054@gmail.com>
2024-08-10 21:11:05 -0600

    feat: web driver wait added

--- examples/theglobaleconomy.py
+++ examples/theglobaleconomy.py
@@ -1,14 +1,14 @@
 from selenium.webdriver.chrome.service import Service as ChromeService
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import Select, WebDriverWait
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import Select
 from selenium import webdriver
-import pandas as pd
 from time import sleep
+import pandas as pd
 
 # Configuración del navegador
 opts = Options()
-
 opts.add_argument("--start-maximized")
 opts.add_argument("--disable-extensions")
 opts.add_argument("--no-sandbox")
@@ -17,27 +17,36 @@ driver = webdriver.Chrome(service=ChromeService(), options=opts)
 
 # Navegar a la página
 driver.get("https://www.theglobaleconomy.com/")
-sleep(5)
-
-# Seleccionar "Rankings"
-element = driver.find_element(By.XPATH, '//*[@id="menu"]/li[6]/a/span')
-element.click()
-sleep(3)
-
-# Seleccionar "Latin America" en el dropdown
-element = driver.find_element(By.XPATH, '//select[@id="regions"]')
 sleep(1)
-dropdown = Select(element)
-sleep(2)
+
+# Esperar a que el elemento "Rankings" sea clickeable y hacer clic
+wait = WebDriverWait(driver, 10)
+rankings_element = wait.until(
+    EC.element_to_be_clickable((By.XPATH, '//*[@id="menu"]/li[6]/a/span'))
+)
+rankings_element.click()
+sleep(1)
+
+# Esperar a que el dropdown esté presente y seleccionable
+dropdown_element = wait.until(
+    EC.presence_of_element_located((By.XPATH, '//select[@id="regions"]'))
+)
+dropdown = Select(dropdown_element)
 dropdown.select_by_visible_text("Latin America")
-sleep(3)
+sleep(1)
+
+# Esperar a que la tabla esté presente antes de extraer los encabezados
+wait.until(EC.presence_of_element_located((By.XPATH, '//*[@id="benchmarkTable"]')))
+sleep(1)
 
 # Extraer los encabezados de la tabla
 headers = driver.find_elements(By.XPATH, '//*[@id="benchmarkTable"]/thead/tr/th')
+sleep(1)
 headers = [header.text for header in headers]
 
 # Extraer datos de la tabla
 rows = driver.find_elements(By.XPATH, '//*[@id="benchmarkTable"]/tbody/tr')
+sleep(1)
 
 data = []
 for row in rows:
@@ -49,6 +58,7 @@ for row in rows:
 df = pd.DataFrame(data, columns=headers)
 
 # Guardar en Excel
-df.to_excel("docs/theglobaleconomy/theglobaleconomy.xlsx", index=False)
+# df.to_excel("docs/theglobaleconomy/theglobaleconomy.xlsx", index=False)
+df.to_excel("theglobaleconomy.xlsx", index=False)
 
 driver.quit()
--- examples/tiobe.py
+++ examples/tiobe.py
@@ -1,4 +1,6 @@
 from selenium.webdriver.chrome.service import Service as ChromeService
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 from selenium import webdriver
@@ -7,44 +9,53 @@ from time import sleep
 
 # Configuración del navegador
 opts = Options()
-
 opts.add_argument("--start-maximized")
 opts.add_argument("--disable-extensions")
 opts.add_argument("--no-sandbox")
 
 driver = webdriver.Chrome(service=ChromeService(), options=opts)
+wait = WebDriverWait(driver, 10)
 
 # Navegar a la página
 driver.get("https://www.tiobe.com/")
-sleep(5)
+sleep(1)
 
-# Denegar cookies
-element = driver.find_element(
-    By.XPATH, '//*[@id="CybotCookiebotDialogBodyButtonDecline"]'
+# Esperar y denegar cookies
+cookie_button = wait.until(
+    EC.element_to_be_clickable(
+        (By.XPATH, '//*[@id="CybotCookiebotDialogBodyButtonDecline"]')
+    )
 )
-element.click()
+cookie_button.click()
 sleep(1)
 
 # Ir a las tablas de popularidad
-element = driver.find_element(By.XPATH, '//li[@id="menu-item-1184"]/a')
-element.click()
-sleep(2)
+menu_item = wait.until(
+    EC.element_to_be_clickable((By.XPATH, '//li[@id="menu-item-1184"]/a'))
+)
+menu_item.click()
+sleep(1)
 
 # Extraer encabezados del top 20
-element = driver.find_element(By.XPATH, "/html/body/section/div/article/p[2]")
-sleep(1)
+element = wait.until(
+    EC.presence_of_element_located((By.XPATH, "/html/body/section/div/article/p[2]"))
+)
 driver.execute_script("arguments[0].scrollIntoView();", element)
 sleep(1)
 
-element = driver.find_element(By.XPATH, "/html/body/section/div/article/p[3]")
-sleep(1)
+element = wait.until(
+    EC.presence_of_element_located((By.XPATH, "/html/body/section/div/article/p[3]"))
+)
 driver.execute_script("arguments[0].scrollIntoView();", element)
 sleep(1)
 
-headers = driver.find_elements(By.XPATH, '//*[@id="top20"]/thead/tr/th')
+headers = wait.until(
+    EC.presence_of_all_elements_located((By.XPATH, '//*[@id="top20"]/thead/tr/th'))
+)
+sleep(1)
 headers = [header.text for header in headers]
-sleep(1)
 
+# Ajustar encabezados eliminando "Change" si es necesario
 first_change_index = headers.index("Change") if "Change" in headers else -1
 if first_change_index != -1:
     headers = [
@@ -53,38 +64,45 @@ if first_change_index != -1:
         if header != "Change" or i > first_change_index
     ]
 
-rows = driver.find_elements(By.XPATH, '//*[@id="top20"]/tbody/tr')
+# Extraer datos de la tabla
+rows = wait.until(
+    EC.presence_of_all_elements_located((By.XPATH, '//*[@id="top20"]/tbody/tr'))
+)
 sleep(1)
-
 data = []
-
 for row in rows:
     cols = row.find_elements(By.TAG_NAME, "td")
     cols = [col.text for col in cols if col.text.strip() != ""]
     data.append(cols)
 
+# Crear DataFrame y guardar en Excel
 df = pd.DataFrame(data, columns=headers)
-df.to_excel("docs/tiobe/Top20.xlsx", index=False)
-sleep(1)
+# df.to_excel("docs/tiobe/Top20.xlsx", index=False)
+df.to_excel("Top20.xlsx", index=False)
 
 # Extraer Otros lenguajes de programación
-element = driver.find_element(By.XPATH, '//*[@id="container"]')
+container = wait.until(
+    EC.presence_of_element_located((By.XPATH, '//*[@id="container"]'))
+)
+driver.execute_script("arguments[0].scrollIntoView();", container)
 sleep(1)
+
+element = wait.until(
+    EC.presence_of_element_located((By.XPATH, "/html/body/section/div/article/h2[1]"))
+)
 driver.execute_script("arguments[0].scrollIntoView();", element)
 sleep(1)
 
-element = driver.find_element(By.XPATH, "/html/body/section/div/article/h2[1]")
+headers = wait.until(
+    EC.presence_of_all_elements_located((By.XPATH, '//*[@id="otherPL"]/thead/tr/th'))
+)
 sleep(1)
-driver.execute_script("arguments[0].scrollIntoView();", element)
-sleep(1)
-
-headers = driver.find_elements(By.XPATH, '//*[@id="otherPL"]/thead/tr/th')
 headers = [header.text for header in headers]
-sleep(1)
 
-rows = driver.find_elements(By.XPATH, '//*[@id="otherPL"]/tbody/tr')
+rows = wait.until(
+    EC.presence_of_all_elements_located((By.XPATH, '//*[@id="otherPL"]/tbody/tr'))
+)
 sleep(1)
-
 data = []
 for row in rows:
     cols = row.find_elements(By.TAG_NAME, "td")
@@ -92,22 +110,28 @@ for row in rows:
     data.append(cols)
 
 df = pd.DataFrame(data, columns=headers)
-df.to_excel("docs/tiobe/OtherProgrammingLanguages.xlsx", index=False)
-sleep(1)
+# df.to_excel("docs/tiobe/OtherProgrammingLanguages.xlsx", index=False)
+df.to_excel("OtherProgrammingLanguages.xlsx", index=False)
 
-# Extraer Lenguajes de programación con mas Historia de muy largo plazo
-element = driver.find_element(By.XPATH, "/html/body/section/div/article/ul[1]/li")
-sleep(1)
+# Extraer Lenguajes de programación con más Historia de muy largo plazo
+element = wait.until(
+    EC.presence_of_element_located(
+        (By.XPATH, "/html/body/section/div/article/ul[1]/li")
+    )
+)
 driver.execute_script("arguments[0].scrollIntoView();", element)
 sleep(1)
 
-headers = driver.find_elements(By.XPATH, '//*[@id="VLTH"]/thead/tr/th')
+headers = wait.until(
+    EC.presence_of_all_elements_located((By.XPATH, '//*[@id="VLTH"]/thead/tr/th'))
+)
+sleep(1)
 headers = [header.text for header in headers]
-sleep(1)
 
-rows = driver.find_elements(By.XPATH, '//*[@id="VLTH"]/tbody/tr')
+rows = wait.until(
+    EC.presence_of_all_elements_located((By.XPATH, '//*[@id="VLTH"]/tbody/tr'))
+)
 sleep(1)
-
 data = []
 for row in rows:
     cols = row.find_elements(By.TAG_NAME, "td")
@@ -115,25 +139,30 @@ for row in rows:
     data.append(cols)
 
 df = pd.DataFrame(data, columns=headers)
-df.to_excel("docs/tiobe/VeryLongTermHistory.xlsx", index=False)
+# df.to_excel("docs/tiobe/VeryLongTermHistory.xlsx", index=False)
+df.to_excel("VeryLongTermHistory.xlsx", index=False)
 
 # Extraer Lenguajes de programación del salón de la fama
-element = driver.find_element(By.XPATH, "/html/body/section/div/article/ul[2]/li[2]")
-sleep(1)
+element = wait.until(
+    EC.presence_of_element_located(
+        (By.XPATH, "/html/body/section/div/article/ul[2]/li[2]")
+    )
+)
 driver.execute_script("arguments[0].scrollIntoView();", element)
 sleep(1)
 
-element = driver.find_element(By.XPATH, "/html/body/section/div/article/p[10]")
-sleep(1)
+element = wait.until(
+    EC.presence_of_element_located((By.XPATH, "/html/body/section/div/article/p[10]"))
+)
 driver.execute_script("arguments[0].scrollIntoView();", element)
 sleep(1)
 
-rows = driver.find_elements(By.XPATH, '//*[@id="PLHoF"]/tbody/tr')
+rows = wait.until(
+    EC.presence_of_all_elements_located((By.XPATH, '//*[@id="PLHoF"]/tbody/tr'))
+)
 sleep(1)
-
 headers = []
 data = []
-
 for i, row in enumerate(rows):
     if i == 0:
         headers = row.find_elements(By.TAG_NAME, "th")
@@ -144,8 +173,7 @@ for i, row in enumerate(rows):
         data.append(cols)
 
 df = pd.DataFrame(data, columns=headers)
-df.to_excel("docs/tiobe/ProgrammingLanguageHallOfFame.xlsx", index=False)
-
-sleep(1)
+# df.to_excel("docs/tiobe/ProgrammingLanguageHallOfFame.xlsx", index=False)
+df.to_excel("ProgrammingLanguageHallOfFame.xlsx", index=False)
 
 driver.quit()
